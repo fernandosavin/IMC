9906779444670d186777bc77f48d0868acd28db7
fernandosavin <38009307+fernandosavin@users.noreply.github.com>
2018-04-07 10:58:30 -0600

    head

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<html lang="es">
+<html lang="es" class="no-js" dir="ltr">
 <head>
 <title>Instituto Mar de Cortés</title>
 <meta charset="utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- Compressed CSS -->
+	<link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
  
