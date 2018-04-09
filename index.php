06928714fe0c805847929d525878328fe6a7c237
edgera17 <38009539+edgera17@users.noreply.github.com>
2018-04-09 11:16:16 -0600

    edgar
    menú
    carrucel

--- index.php
+++ index.php
@@ -12,104 +12,104 @@
 	<link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-
+    <link rel="stylesheet" type="text/css" href="css/styleCarrucel.css">
+    <link rel="stylesheet" type="text/css" href="css/navBar.css">
 </head>
  
 <body>
     <header>
-       
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="navigation">
+                <div class="container">                 
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse">
+                            <span class="sr-only">NavBar</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <div class="navbar-brand">
+                            <a href="index.html"><img src="http://www.institutomardecortes.edu.mx/images/LogoMarDeCortes_transparent.png" width="50px"></a>
+                        </div>
+                    </div>
+                    
+                    <div class="navbar-collapse collapse">                          
+                        <div class="menu">
+                            <ul class="nav nav-tabs" role="tablist">
+                                <li role="presentation"><a href="#" class="active">Inicio</a></li>
+                                <li role="presentation"><a href="#">Conócenos</a></li>
+                                <li role="presentation"><a href="#">Red Estudiantil</a></li>   
+                                <li role="presentation"><a href="#">Noticias</a></li>  
+                                <li role="presentation"><a href="#">Galerías</a></li>                            
+                                <li role="presentation"><a href="#">Enlaces de Interes</a></li>
+                                <li role="presentation"><a href="#">Contáctanos</a></li>                    
+                            </ul>
+                        </div>
+                    </div>                      
+                </div>
+            </div>  
+        </nav>
 
     </header>
-    <section>
-      
+
+    <section class="eventos container">
+        
+            <div class="col-sm-6 carrucelDiv">
+                <div class="container text-center div2">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                       
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                          <li data-target="#myCarousel" data-slide-to="1"></li>
+                          <li data-target="#myCarousel" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+
+                            <div class="item active divCarrucel">
+                                <img src="http://www.institutomardecortes.edu.mx/media/galerias/2018-A/14%20de%20febrero/DSCN0295.JPG" alt="Createc" class="imgCarrucel">
+                                <div class="carousel-caption textCarru">
+                                    <h3>Createc</h3>
+                                    <p>Exposición de proyectos creativos.</p>
+                                </div>
+                            </div>
+
+                            <div class="item divCarrucel">
+                                <img src="http://www.institutomardecortes.edu.mx/media/galerias/2018-A/XX%20Aniversario%20del%20Instituto%20Mar%20de%20Cort%C3%A9s/DSCN0913.JPG" alt="Multimedia" class="imgCarrucel">
+                                <div class="carousel-caption textCarru">
+                                    <h3>Multimedia</h3>
+                                    <p>Exposición de proyectos multimedia</p>
+                                </div>
+                             </div>
+                          
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-sm-6 lineaTiempo">
+                <h1>Time Line</h1>
+            </div>
+              
     </section>
+
+        
     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
 
 
 
@@ -198,24 +198,7 @@
 
 
     <aside>
-      <div class="row medium-unstack">
-    <div class="medium-4 columns">
-		<!-- InstaWidget -->
-<a href="https://instawidget.net/v/user/prepamardecortes" id="link-d4256a6f05bfa90e8bac6f2883f0953d713589962214964a61086185a9011bdd">@prepamardecortes</a>
-<script src="https://instawidget.net/js/instawidget.js?u=d4256a6f05bfa90e8bac6f2883f0953d713589962214964a61086185a9011bdd&width=300px"></script>
-    </div>
-
-    <div class="medium-4 columns">
-      <a class="twitter-timeline" data-width="340" data-height="500" href="https://twitter.com/instmardecortes?ref_src=twsrc%5Etfw">Tweets by instmardecortes</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div>
-
-	<div class="medium-4 columns">
-		<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FInstMardeCortes%2F&tabs=timeline&width=320&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-	</div>
-
-  </div>
-  </div>
-
+     
     </aside>
     <footer>
         
